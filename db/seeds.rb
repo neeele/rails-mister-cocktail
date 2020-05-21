4685a63d7259750eda899617bdb8c7599f1813aa
Cornelia <neele@protonmail.com>
2020-05-21 16:50:31 +0200

    Apparently frontend has to wait

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,20 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+require 'open-uri'
+require 'json'
+
+puts 'Creating drinks...'
+
+url = 'https://www.thecocktaildb.com/api/json/v1/1/list.php?i=list'
+ingredients_serialized_ = open(url).read
+ingredients = JSON.parse(ingredients_serialized_)
+
+x = 0
+
+ingredients["drinks"].each do |ingredient|
+  ing = Ingredient.create(name: ingredient["strIngredient1"])
+  x += 1
+end
+puts "Created #{x} seeds!"
